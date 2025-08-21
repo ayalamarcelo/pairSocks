a18c1762535d6ff2c115a4ad95f40fe2fcc01c11
Marcelo <marcelorubenayala@outlook.com>
2025-08-21 15:07:02 -0300

    Initial commit :tada:

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "medias-app-cache-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./script.js",
+  "https://cdn.sheetjs.com/xlsx-latest/package/dist/xlsx.full.min.js"
+  // Agregá aquí otros archivos si los necesitás cachear
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
